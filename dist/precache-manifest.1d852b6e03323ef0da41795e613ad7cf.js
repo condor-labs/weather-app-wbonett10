d07df1723c71ba157b538c6f104f61d8ddedf631
edwin-rodriguez <edwin-rodriguez@users.noreply.github.com>
2020-08-20 19:51:22 -0500

    dist upload

---
+++ dist/precache-manifest.1d852b6e03323ef0da41795e613ad7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa511baeadefca2d5c6e1f5facfd652",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e1fadf125396bef472e",
+    "url": "/static/css/main.0ddbe53c.chunk.css"
+  },
+  {
+    "revision": "58690094c38798d15bd8",
+    "url": "/static/js/2.c6763569.chunk.js"
+  },
+  {
+    "revision": "360978b69b2d355b7f6ed150ede4e119",
+    "url": "/static/js/2.c6763569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1fadf125396bef472e",
+    "url": "/static/js/main.66d546aa.chunk.js"
+  },
+  {
+    "revision": "c440ec29c1f2aa7e14f3",
+    "url": "/static/js/runtime-main.86ba2e87.js"
+  }
+]);
